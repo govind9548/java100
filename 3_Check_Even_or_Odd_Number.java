6758ba8b40ffa4672ac48adc3fa98af9a23cdb30
Govind <167590582+govind9548@users.noreply.github.com>
2025-01-08 21:33:45 +0530

    Create 3_Check_Even_or_Odd_Number.java

---
+++ 3_Check_Even_or_Odd_Number.java
@@ -0,0 +1,15 @@
+// Check Even or Odd Number
+// T.C  ---> O(1)
+// S.C  ---> O(1)
+
+class Main {
+    public static void main(String[] args) {
+        // System.out.println("Try programiz.pro");
+        int a=10;
+        if(a%2==0){
+            System.out.println("Yes, Even");
+        }else{
+            System.out.println("No, Odd");
+        }
+    }
+}
